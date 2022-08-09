83597657b5ce26a99270de796a03eb4e5d200d58
josimar120 <josimar.17perseu@gmail.com>
2022-08-09 16:10:18 -0300

    feat:(rota de pegar usuario com autenticação concluida)

--- src/controllers/visitLinkController.js
+++ src/controllers/visitLinkController.js
@@ -2,6 +2,21 @@ import connection from "../database/db.js";
 
 export async function getUsers(req, res){
 
+    const idUser = res.locals.idUsers;
+
+    const{rows:user} = await connection.query(`SELECT u.id, u.name, 
+        SUM(l."visitLink") AS "visitCount"
+        FROM users u
+        JOIN links l ON $1 = u.id
+        GROUP BY u.id`, [idUser]);
+
+    const {rows:link} = await connection.query(`SELECT l."shortLink", l."longLink", l."visitLink"
+    FROM links l 
+    WHERE l."idUsers" = $1`, [idUser]);
+
+    user[0].shortenedUrls = link;
+
+    res.status(200).send(user[0]);
 }
 
 export async function getRanking(req, res){
@@ -15,8 +30,6 @@ export async function getRanking(req, res){
         ORDER BY "linksCount" DESC
         LIMIT 10` );
 
-    console.log(ranking);
-
-    res.status(200).send("ok");
+    res.status(200).send(ranking);
     
 }
--- src/routes/visitLinkRouter.js
+++ src/routes/visitLinkRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import {getUsers, getRanking} from '../controllers/visitLinkController.js'
+import {getUsers, getRanking} from '../controllers/visitLinkController.js';
+import authorizationToken from '../middlewares/authorizationToken.js';
 
 const visitLinkRouter = Router();
 
-visitLinkRouter.get('/users/me', getUsers);
+visitLinkRouter.get('/users/me', authorizationToken, getUsers);
 visitLinkRouter.get('/ranking', getRanking);
 
 export default visitLinkRouter;
